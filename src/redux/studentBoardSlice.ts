31422752178c2ab892c96aafc043c98c246691a3
romeo <romeomeshack34@gmail.com>
2022-05-20 18:59:17 +0100

    Redux slice

--- src/redux/studentBoardSlice.ts
+++ src/redux/studentBoardSlice.ts
@@ -3,18 +3,17 @@ import {createSlice} from "@reduxjs/toolkit";
 interface RecordStateType {
     studentName: string,
     studentsRecord: any,
-    studentClasses: {name: string, classes: string[]},
+    studentClasses: null | string[],
+    students: null | string[],
 }
 
 const recordState: string | null = localStorage.getItem("RECORD_STATE");
 
 let initialState = {
     studentName: "",
-    studentsRecord: [],
-    studentClasses: {
-        name: "",
-        classes: []
-    }
+    studentsRecord: null,
+    studentClasses: null,
+    students: null
 } as RecordStateType
 
 if (typeof recordState === "string") {
@@ -41,11 +40,24 @@ export const schoolSlice = createSlice({
             };
             localStorage.setItem("RECORD_STATE", JSON.stringify(updateState));
         },
-        setClassesRecord: (state, action) => {
-            state.studentsRecord = action.payload;
+        setStudentClasses: (state, action) => {
+            state.studentClasses = action.payload;
+            const updateState = {
+                ...initialState,
+                studentClasses:  action.payload
+            };
+            localStorage.setItem("RECORD_STATE", JSON.stringify(updateState));
         },
+        setStudents: (state, action) => {
+            state.students = action.payload;
+        }
     }
 });
 
-export const {setStudentName, setStudentsRecord, setClassesRecord} = schoolSlice.actions;
+export const {
+    setStudentName, 
+    setStudentsRecord, 
+    setStudentClasses,
+    setStudents
+} = schoolSlice.actions;
 export default schoolSlice.reducer;
