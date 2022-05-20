1a6e9c4b253659e7a8fdceeceb83d27eeecc8809
romeo <romeomeshack34@gmail.com>
2022-05-20 18:59:49 +0100

    Board component

--- src/Board.tsx
+++ src/Board.tsx
@@ -6,27 +6,93 @@ import { logout } from './redux/authSlice';
 export default function Board() {
 
   const dispatch = useAppDispatch()
-  const {fetchClassRecord} = useAirtableClient()
+  const { fetchClassRecord, fetchStudentInClasses } = useAirtableClient()
 
-  const studentRecordState = useAppSelector(state => state.school.studentsRecord);
   const studentName = useAppSelector(state => state.school.studentName);
+  const studentClasses: null | string[] = useAppSelector(state => state.school.studentClasses);
+  const students = useAppSelector(state => state.school.students);
 
   useEffect(() => {
-    if (studentName !== "") {
+    if (studentName !== "" && studentClasses === null) {
       fetchClassRecord(studentName);
-      console.log(studentName);
     }
-  }, [fetchClassRecord, studentRecordState, studentName])
+  }, [fetchClassRecord, studentName, studentClasses])
+
+  useEffect(() => {
+    if (students === null) {
+
+      let formula: string = "";
+
+      const createFormula = (c: string, index: number) => {
+
+        formula += `SEARCH('${c}', {Classes}),`
+
+        if (studentClasses?.length === 1) formula = `SEARCH('${c}', {Classes})`;
+
+        if (studentClasses?.length !== 1 && (index + 1) === studentClasses?.length) {
+          formula += `SEARCH('${c}', {Classes})`;
+        }
+
+        console.log(formula);
+      }
+
+      studentClasses?.forEach(createFormula);
+
+      if (formula !== "") fetchStudentInClasses(formula);
+    }
+  },[studentClasses, students, fetchStudentInClasses])
+  
+
+  const Classes = (props: any) => {
+    const classes = props.classes;
+
+    if (classes !== null && classes?.length > 0) {
+     return classes.map((c: any, index: number) => (
+       <>
+       <small key={index}>{`${c}`}</small>{" "}
+       </>
+     ));
+    } else if(classes !== null && classes?.length > 0) {
+     return (<p>{classes[0]}</p>); 
+    }
+
+    return null
+  }
+
+  const StudentsComponent = (props: any) => {
+    const students = props.students;
+
+    if (students !== null) {
+      return students.map((student: string, index: number) => (
+        <>
+          <small key={index}>{student}</small>
+          {" "}
+        </>
+      ))
+    }
+
+    return null
+  }
+
+  const logoutFunc = () => {
+    const action = dispatch(logout());
+
+    if (action.type === "auth/logout") window.location.reload();
+  }
 
   return (
     <div>
-      <button id="logout" onClick={() => dispatch(logout())} style={{ float: "right" }}>Logout</button>
+      <button id="logout" onClick={logoutFunc} style={{ float: "right" }}>Logout</button>
 
       <div id="board">
-        <strong className='board-header'>Name</strong>
-        <p className='board-item'>cs 101</p>
-        <strong className='board-header'>Students</strong>
-        <p className='board-item'>James, Emmanuel, Mark, Uche</p>
+        <strong className='board-header'>Names of classes</strong>
+        <div>
+          <Classes classes={studentClasses} />
+        </div>
+        <strong className='board-header'>Students in classes</strong>
+        <div>
+        <StudentsComponent students={students} />
+        </div>
       </div>
     </div>
   )
